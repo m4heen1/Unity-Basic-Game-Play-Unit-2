f9400c19ea58388bc6fc5abc6cb9a1d7793feb8d
m4heen1 <bhuim263@hsd.k12.or.us>
2025-04-10 21:01:11 -0700

    Lesson 2.2 Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerControl : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,5 +29,11 @@ public class PlayerControl : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); 
+        }
     }
 }
