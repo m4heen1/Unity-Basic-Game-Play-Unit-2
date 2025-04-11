b89bfde1048cbdd156c64aed8cc0cbf887632c70
m4heen1 <bhuim263@hsd.k12.or.us>
2025-04-10 22:01:49 -0700

    Lesson 2.4 Done

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -18,7 +18,8 @@ public class DestroyOutOfBounds : MonoBehaviour
             Destroy(gameObject);
        } else if (transform.position.z < lowerBound)
        {
-            Destroy(gameObject);
+          Debug.Log("Game Over!");
+          Destroy(gameObject);
        }
     }
 }
